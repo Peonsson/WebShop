a28ced9fbd2e62d7647099b02eb1e5521ffa74dc
roppe546 <roppe546@gmail.com>
2015-10-01 23:30:56 +0200

    Edit item view now reads data from backend (category not reading yet).

--- src/Servlets/GetItem.java
+++ src/Servlets/GetItem.java
@@ -9,20 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import BusinessLogic.Item;
 import BusinessLogic.ItemHandler;
 
-@WebServlet("/GetItem")
+//@WebServlet("/GetItem")
 public class GetItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     public GetItem() {
         super();
     }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//    	request.getParameter("itemId"); //TODO: in production
-//    	System.out.println("ItemHandler.getItem(1).getName() == " + ItemHandler.getItem(1).getName()); //TODO: testcode remove if u want
-    	System.out.println("ItemHandler.getItem(1).getName() == " + ItemHandler.getItem("Ficklampa").getName()); //TODO: testcode remove if u want
-//    	request.getParameter("name");//TODO: in production
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ 		int itemId = Integer.parseInt(request.getParameter("itemId"));
+ 		Item item = ItemHandler.getItem(itemId);
+ 		request.setAttribute("item", item);
+ 		request.getRequestDispatcher("/Pages/ItemEditor.jsp").forward(request, response);
 	}
 }
